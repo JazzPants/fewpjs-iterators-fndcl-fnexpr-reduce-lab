01a77889ec6f549c405f461fe085e239ace371a0
JazzPants <leighton.l14@gmail.com>
2021-11-23 02:40:36 +1100

    Done!

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce((accumulator, element) => {
+    return element + accumulator;
+}, 0)
+
+//accumulator starts at 0, and keeps getting element added to it as the reduce function
+//loops through all the elements in the array. "Accumulates" the value
+
+//element is the array value passed in by reduce, it goes through every element
+
+// better to initialise the total at 0, especially if you are doing more operations on the
+// array element
+
+
+console.log(totalBatteries);
+
+
+//LAB SOLUTION
+
+// const totalBatteries = batteryBatches.reduce(function(total, batches){
+//   return batches + total
+//   }
+// )
